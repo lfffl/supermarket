90ffe705c5c5d0aa112981d178489c21e823687e
Franco <frankantonio17@gmail.com>
2020-07-17 20:14:31 -0400

    prueba2

---
+++ src/pages/mapa.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Mapa extends StatefulWidget {
+  @override
+  _MapaState createState() => _MapaState();
+}
+
+class _MapaState extends State<Mapa> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Supermecados cercanos"),),
+      
+    );
+  }
+}
